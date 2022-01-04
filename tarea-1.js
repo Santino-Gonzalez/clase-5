138ad6efff9fdc8ee78e1e610a8a7d37369c7739
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2022-01-04 01:34:44 -0300

    clase-5-tarea-1
    Esta es la tarea 1 de la clase 5

---
+++ tarea-1.js
@@ -0,0 +1,19 @@
+//TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+function calcularSalarioMensual(salarioAnual){
+    const mesesDelAnio = 12
+    return salarioAnual / mesesDelAnio
+  }
+  
+  const $calcularSalarioMensual = document.querySelector("#calcular-salario-mensual")
+  
+  $calcularSalarioMensual.onclick = function(){
+    const salarioAnual = Number(document.querySelector("#salario-anual").value)
+    const salarioMensual = calcularSalarioMensual(salarioAnual)
+    document.querySelector("#salario-mensual").value = salarioMensual
+    
+    return false
+  }
