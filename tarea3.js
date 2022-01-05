25a81713bc90cc0db2f416327551965d29448f07
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2022-01-05 10:25:33 -0300

    Mejora de código
    Cambie el código para que no sea necesario cambiarlo si quisiera agregar tiempos de mas clases

--- tarea3.js
+++ tarea3.js
@@ -1,38 +1,22 @@
-const $calculateTotalTime = document.querySelector("#calculate-total-time")
-const $totalTimeText = document.querySelector("h1")
 
-function calculateTotalHours (){
-    const hoursClass1 = Number(document.querySelector("#first-class-hours").value)
-    const hoursClass2 = Number(document.querySelector("#second-class-hours").value)
-    const hoursClass3 = Number(document.querySelector("#third-class-hours").value)
-    const hoursClass4 = Number(document.querySelector("#fourth-class-hours").value)
-    const hoursClass5 = Number(document.querySelector("#fifth-class-hours").value)
-    let hour = hoursClass1 + hoursClass2 + hoursClass3 + hoursClass4 + hoursClass5
-    return hour
-}
-function calculateTotalMinutes (){
-    const minutesClass1 = Number(document.querySelector("#first-class-minutes").value)
-    const minutesClass2 = Number(document.querySelector("#second-class-minutes").value)
-    const minutesClass3 = Number(document.querySelector("#third-class-minutes").value)
-    const minutesClass4 = Number(document.querySelector("#fourth-class-minutes").value)
-    const minutesClass5 = Number(document.querySelector("#fifth-class-minutes").value)
-    let minutes = minutesClass1 + minutesClass2 + minutesClass3 + minutesClass4 + minutesClass5
-    return minutes
-}
-function calculateTotalSeconds (){
-    const secondsClass1 = Number(document.querySelector("#first-class-seconds").value)
-    const secondsClass2 = Number(document.querySelector("#second-class-seconds").value)
-    const secondsClass3 = Number(document.querySelector("#third-class-seconds").value)
-    const secondsClass4 = Number(document.querySelector("#fourth-class-seconds").value)
-    const secondsClass5 = Number(document.querySelector("#fifth-class-seconds").value)
-    let seconds = secondsClass1 + secondsClass2 + secondsClass3 + secondsClass4 + secondsClass5
-    return seconds
-}
+const $calculateTotalTime = document.querySelector("#calculate-total-time")
+const $totalTimeText = document.querySelector("strong")
 
 $calculateTotalTime.onclick = function(){
-    let totalSeconds = calculateTotalSeconds()
-    let totalMinutes  = calculateTotalMinutes()
-    let totalHour = calculateTotalHours()
+    const hours = document.querySelectorAll(".hours")
+    const minutes = document.querySelectorAll(".minutes")
+    const seconds = document.querySelectorAll (".seconds")
+    let totalHours = calculateTime(hours)
+    let totalMinutes = calculateTime(minutes)
+    let totalSeconds = calculateTime(seconds)
+
+    function calculateTime(time) {
+    let finalTime = 0;
+    for (let i = 0; i < time.length; i++) {
+      finalTime += Number(time[i].value);
+    }
+    return finalTime;
+  }
 
     if(totalSeconds >= 60){
         while(totalSeconds >= 60){
@@ -44,9 +28,10 @@ $calculateTotalTime.onclick = function(){
     if(totalMinutes >= 60){
         while(totalMinutes >= 60){
             totalMinutes = totalMinutes - 60
-            totalHour++
+            totalHours++
         }
     }
 
-    $totalTimeText.textContent = `El tiempo total es de ${totalHour}hs ${totalMinutes}mins ${totalSeconds}seg`
+
+    $totalTimeText.textContent = `El tiempo total es de ${totalHours}hs ${totalMinutes}mins ${totalSeconds}seg`
 }
