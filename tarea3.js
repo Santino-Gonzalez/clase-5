650a2d2ba639b9848015fb3d2793980293a6666d
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2022-01-04 21:41:15 -0300

    Corrección de error resultado "undefined"
    Debido a un error en el código el resultado se mostraba como undefined en lugar de su correcto valor

--- tarea3.js
+++ tarea3.js
@@ -7,7 +7,8 @@ function calculateTotalHours (){
     const hoursClass3 = Number(document.querySelector("#third-class-hours").value)
     const hoursClass4 = Number(document.querySelector("#fourth-class-hours").value)
     const hoursClass5 = Number(document.querySelector("#fifth-class-hours").value)
-    hoursClass1 + hoursClass2 + hoursClass3 + hoursClass4 + hoursClass5
+    let hour = hoursClass1 + hoursClass2 + hoursClass3 + hoursClass4 + hoursClass5
+    return hour
 }
 function calculateTotalMinutes (){
     const minutesClass1 = Number(document.querySelector("#first-class-minutes").value)
@@ -15,7 +16,8 @@ function calculateTotalMinutes (){
     const minutesClass3 = Number(document.querySelector("#third-class-minutes").value)
     const minutesClass4 = Number(document.querySelector("#fourth-class-minutes").value)
     const minutesClass5 = Number(document.querySelector("#fifth-class-minutes").value)
-    minutesClass1 + minutesClass2 + minutesClass3 + minutesClass4 + minutesClass5
+    let minutes = minutesClass1 + minutesClass2 + minutesClass3 + minutesClass4 + minutesClass5
+    return minutes
 }
 function calculateTotalSeconds (){
     const secondsClass1 = Number(document.querySelector("#first-class-seconds").value)
@@ -23,12 +25,15 @@ function calculateTotalSeconds (){
     const secondsClass3 = Number(document.querySelector("#third-class-seconds").value)
     const secondsClass4 = Number(document.querySelector("#fourth-class-seconds").value)
     const secondsClass5 = Number(document.querySelector("#fifth-class-seconds").value)
-    secondsClass1 + secondsClass2 + secondsClass3 + secondsClass4 + secondsClass5
+    let seconds = secondsClass1 + secondsClass2 + secondsClass3 + secondsClass4 + secondsClass5
+    return seconds
 }
 
 $calculateTotalTime.onclick = function(){
     let totalSeconds = calculateTotalSeconds()
-    
+    let totalMinutes  = calculateTotalMinutes()
+    let totalHour = calculateTotalHours()
+
     if(totalSeconds >= 60){
         while(totalSeconds >= 60){
             totalSeconds = totalSeconds - 60
@@ -36,16 +41,12 @@ $calculateTotalTime.onclick = function(){
         }
     }
 
-    let totalMinutes  = calculateTotalMinutes()
-
     if(totalMinutes >= 60){
         while(totalMinutes >= 60){
             totalMinutes = totalMinutes - 60
             totalHour++
         }
     }
-
-    let totalHour = calculateTotalHours()
 
     $totalTimeText.textContent = `El tiempo total es de ${totalHour}hs ${totalMinutes}mins ${totalSeconds}seg`
 }
