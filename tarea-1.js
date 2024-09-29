2030541c7ee99819bf77d5ed08f88f99ee1f4e3b
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-09-29 18:50:41 -0300

    Agrega funcionalidad
    Se escribe el código que calcule el salario mensual del usuario.

--- tarea-1.js
+++ tarea-1.js
@@ -2,3 +2,15 @@
 //TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+const $salarioAnual = document.querySelector("#salarioAnual");
+const $salarioMensual = document.querySelector("#salarioMensual");
+
+function calcularSalarioMensual(salarioAnual){
+  return salarioAnual / 12;
+}
+
+document.querySelector("#botonCalcular").onclick = function(){
+  $salarioMensual.value = calcularSalarioMensual($salarioAnual.value);
+  return false;
+}
