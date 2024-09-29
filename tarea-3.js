dd79e1657a77db763c1c40e84b92acddf729e6fa
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-09-29 18:58:09 -0300

    Agrega funcionalidad
    Se agrega el código que calcula el total de la duración de los videos.

--- tarea-3.js
+++ tarea-3.js
@@ -5,3 +5,29 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+document.querySelector("#botonCalcular").onclick = function (){
+    let totalHoras = 0;
+    let totalMinutos = 0;
+    let totalSegundos = 0;
+
+    for (let i = 1; i < 6; i++) {
+        totalHoras = totalHoras + Number(document.querySelector(`#clase${i}Horas`).value);
+        totalMinutos = totalMinutos + Number(document.querySelector(`#clase${i}Minutos`).value);
+        totalSegundos = totalSegundos + Number(document.querySelector(`#clase${i}Segundos`).value);
+    }
+
+    if(totalSegundos > 60){
+        totalMinutos = totalMinutos + Math.floor(totalSegundos / 60);
+        totalSegundos = totalSegundos % 60;
+    }
+
+    if(totalMinutos > 60){
+        totalHoras = totalHoras + Math.floor(totalMinutos / 60);
+        totalMinutos = totalMinutos % 60;
+    }
+
+    document.querySelector("#tiempoTotal").innerHTML = `${totalHoras}hrs ${totalMinutos}mins ${totalSegundos}segs`
+    
+    return false;
+}
